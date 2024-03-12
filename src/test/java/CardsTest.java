12c83543c1d6dc292198278316a4a1638787c068
Justin Condello <jcondello@yahoo.com>
2024-03-12 17:09:06 +1100

    Fixed the test

--- src/test/java/CardsTest.java
+++ src/test/java/CardsTest.java
@@ -1,11 +1,16 @@
 import org.jcps.SetDeck;
+import org.junit.jupiter.api.Test;
 
-public class testCards {
-    public static void main(String[] args) {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardsTest {
+    @Test
+    public void cardTest() {
         final SetDeck setDeck = new SetDeck();
         setDeck.print();
         System.out.println("---------- Shuffling ---------------");
         setDeck.shuffle();
         setDeck.print();
+        assertEquals(81, setDeck.remaining());
     }
 }
